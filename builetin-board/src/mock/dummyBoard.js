9cdf364c1cf03b23972d8b843b18297290b44b70
demetoir-destop <wnsqlehlswk@naver.com>
2020-02-03 19:49:50 +0900

    add: dummyBoard data for bulletin-board

---
+++ builetin-board/src/mock/dummyBoard.js
@@ -0,0 +1,64 @@
+import React from 'react';
+
+const dummyBoard = {
+    posts: [
+        {
+            id: 1,
+            title: <a href=".">{'title'}</a>,
+            date: new Date().toLocaleString(),
+            userName: 'userName',
+            stars: 4,
+            views: 3,
+            replies: 4,
+        },
+        {
+            id: 1,
+            title: <a href=".">{'title'}</a>,
+            date: new Date().toLocaleString(),
+            userName: 'userName',
+            stars: 4,
+            views: 3,
+            replies: 4,
+        },
+        {
+            id: 1,
+            title: <a href=".">{'title'}</a>,
+            date: new Date().toLocaleString(),
+            userName: 'userName',
+            stars: 4,
+            views: 3,
+            replies: 4,
+        },
+        {
+            id: 1,
+            title: <a href=".">{'title'}</a>,
+            date: new Date().toLocaleString(),
+            userName: 'userName',
+            stars: 4,
+            views: 3,
+            replies: 4,
+        },
+        {
+            id: 1,
+            title: <a href=".">{'title'}</a>,
+            date: new Date().toLocaleString(),
+            userName: 'userName',
+            stars: 4,
+            views: 3,
+            replies: 4,
+        },
+        {
+            id: 1,
+            title: <a href=".">{'title'}</a>,
+            date: new Date().toLocaleString(),
+            userName: 'userName',
+            stars: 4,
+            views: 3,
+            replies: 4,
+        },
+    ],
+    postCount: 10,
+    currentPage: 4,
+};
+
+export default dummyBoard;
