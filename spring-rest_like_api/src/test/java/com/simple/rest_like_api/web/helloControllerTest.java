0e9e4b45dfc9791018ae14bc1829d9d4333b46d5
demetoir-destop <wnsqlehlswk@naver.com>
2020-02-06 21:56:30 +0900

    fix: spring controller test by MVC mock example

--- spring-rest_like_api/src/test/java/com/simple/rest_like_api/web/helloControllerTest.java
+++ spring-rest_like_api/src/test/java/com/simple/rest_like_api/web/helloControllerTest.java
@@ -6,12 +6,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-import static org.hamcrest.Matchers.is;
 
 @RunWith(SpringRunner.class)
-@WebMvcTest
+// method 1
+// ref https://mkyong.com/spring-boot/spring-rest-integration-test-example/
+// @SpringBootTest
+// @AutoConfigureMockMvc
+// method2
+// ref https://meetup.toast.com/posts/124
+@WebMvcTest(helloController.class)
 class helloControllerTest {
 
   @Autowired private MockMvc mvc;
