5f900a891be2ecf8977d39027d31ee871379c3a8
demetoir-destop <wnsqlehlswk@naver.com>
2020-02-06 17:43:52 +0900

    chore: clean code

--- spring-rest_like_api/src/test/java/com/simple/rest_like_api/service/posts/PostsApiControllerTest.java
+++ spring-rest_like_api/src/test/java/com/simple/rest_like_api/service/posts/PostsApiControllerTest.java
@@ -16,7 +16,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
