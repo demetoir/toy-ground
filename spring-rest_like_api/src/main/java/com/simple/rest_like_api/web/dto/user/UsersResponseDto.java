4fea4daef7ddf03c5348a471d82e70a9e74523ee
demetoir-destop <wnsqlehlswk@naver.com>
2020-02-08 01:59:53 +0900

    squash to user implementation as clean code + setter

--- spring-rest_like_api/src/main/java/com/simple/rest_like_api/web/dto/user/UsersResponseDto.java
+++ spring-rest_like_api/src/main/java/com/simple/rest_like_api/web/dto/user/UsersResponseDto.java
@@ -1,11 +1,12 @@
 package com.simple.rest_like_api.web.dto.user;
 
 import com.simple.rest_like_api.domain.user.Users;
-import com.simple.rest_like_api.domain.user.UsersRepository;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
+@Setter
 @NoArgsConstructor
 public class UsersResponseDto {
   private Long id;
--- spring-rest_like_api/src/main/java/com/simple/rest_like_api/web/dto/user/UsersUpdateRequestDto.java
+++ spring-rest_like_api/src/main/java/com/simple/rest_like_api/web/dto/user/UsersUpdateRequestDto.java
@@ -1,22 +1,12 @@
 package com.simple.rest_like_api.web.dto.user;
 
-import com.simple.rest_like_api.domain.user.Users;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
+import lombok.Setter;
 
 @Getter
+@Setter
 @NoArgsConstructor
 public class UsersUpdateRequestDto {
   private String name;
-
-  @Builder
-  public UsersUpdateRequestDto(String name) {
-    this.name = name;
-  }
-
-  public Users toEntity() {
-    return Users.builder().name(this.name).build();
-  }
 }
