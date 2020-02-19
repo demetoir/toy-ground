c28d9b397f26a7744984ce9f6073ddcb37f9cfc6
demetoir-destop <wnsqlehlswk@naver.com>
2020-02-20 02:17:02 +0900

    add: spring example naver OAuth2.0 login out

--- spring-rest_like_api/src/main/java/com/simple/rest_like_api/config/auth/dto/OAuthAttributes.java
+++ spring-rest_like_api/src/main/java/com/simple/rest_like_api/config/auth/dto/OAuthAttributes.java
@@ -32,6 +32,11 @@ public class OAuthAttributes {
   // registeredId 는 oauth 종류 구분하는 인자
   public static OAuthAttributes of(
       String registeredId, String userNameAttributeName, Map<String, Object> attributes) {
+
+    if ("naver".equals(registeredId)) {
+      return ofNaver("id", attributes);
+    }
+
     return ofGoogle(userNameAttributeName, attributes);
   }
 
@@ -46,8 +51,23 @@ public class OAuthAttributes {
         .build();
   }
 
+  public static OAuthAttributes ofNaver(
+      String userNameAttributeName, Map<String, Object> attributes) {
+
+    System.out.println(attributes);
+    Map<String, Object> response = (Map<String, Object>) attributes.get("response");
+
+    return OAuthAttributes.builder()
+        .name((String) response.get("name"))
+        .email((String) response.get("email"))
+        .picture((String) response.get("profileImage"))
+        .attributes(response)
+        .nameAttributeKey(userNameAttributeName)
+        .build();
+  }
+
   public User toEntity() {
-    //일단 테스트를 위해 role 을 guest 에서 user로 변경함
+    // 일단 테스트를 위해 role 을 guest 에서 user로 변경함
     return User.builder().name(name).email(email).picture(picture).role(Role.USER).build();
   }
 }
