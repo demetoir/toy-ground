90a9e76e4be4f4a45a2ebde6b49556a3bf65d719
demetoir-destop <wnsqlehlswk@naver.com>
2020-02-20 01:40:59 +0900

    add: spring example sessionUser annotation
    controller 에서 sessionUser를 사용할때 인자로 주입받도록 annotation 추가

--- spring-rest_like_api/src/main/java/com/simple/rest_like_api/web/IndexController.java
+++ spring-rest_like_api/src/main/java/com/simple/rest_like_api/web/IndexController.java
@@ -1,30 +1,22 @@
 package com.simple.rest_like_api.web;
 
-
+import com.simple.rest_like_api.config.auth.LoginUser;
 import com.simple.rest_like_api.config.auth.dto.SessionUser;
-import com.simple.rest_like_api.domain.user.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.mail.Session;
-import javax.servlet.http.HttpSession;
-
 @RequiredArgsConstructor
 @RestController
 public class IndexController {
 
-  private final HttpSession httpSession;
-
   @GetMapping("/")
-  public String index(Model model) {
-    SessionUser user = (SessionUser) httpSession.getAttribute("user");
-
+  public String index(Model model, @LoginUser SessionUser user) {
     if (user != null) {
-
       return "logined as" + user.getName();
     }
+
     return "not logined";
   }
 }
