ca6b9c977d5d7212f2060f033c73bfc41f161e71
demetoir-destop <wnsqlehlswk@naver.com>
2020-02-20 23:12:35 +0900

    add: init project of start spring boot

---
+++ start-spring-boot/src/main/java/com/example/demetoir/DemetoirApplication.java
@@ -0,0 +1,13 @@
+package com.example.demetoir;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemetoirApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemetoirApplication.class, args);
+    }
+
+}
---
+++ start-spring-boot/src/test/java/com/example/demetoir/DemetoirApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demetoir;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemetoirApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
