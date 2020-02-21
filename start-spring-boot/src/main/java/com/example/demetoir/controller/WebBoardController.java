437552fc446a9d3d481430211e33deb59c63a744
demetoir-destop <wnsqlehlswk@naver.com>
2020-02-21 23:20:24 +0900

    add: spring example, start spring boot p.313
    게시물 등록 기능 추가

--- start-spring-boot/src/main/java/com/example/demetoir/controller/WebBoardController.java
+++ start-spring-boot/src/main/java/com/example/demetoir/controller/WebBoardController.java
@@ -11,6 +11,9 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Log
 @Controller
@@ -27,8 +30,8 @@ public class WebBoardController {
   public void list(PageVO pageVO, Model model) {
 
     Pageable page = pageVO.makePageable(0, "bno");
-    Page<WebBoard> result = repo.findAll(repo.makePredicate(pageVO.getType(), pageVO.getKeyword()), page);
-
+    Page<WebBoard> result =
+        repo.findAll(repo.makePredicate(pageVO.getType(), pageVO.getKeyword()), page);
 
     log.info("page " + page);
     log.info("result " + result);
@@ -38,6 +41,20 @@ public class WebBoardController {
     PageMaker<WebBoard> resultPage = new PageMaker<>(result);
     log.info("" + resultPage.getPrevPage());
     model.addAttribute("result", resultPage);
+  }
+
+  @GetMapping("/boards/register")
+  public void registerGET(@ModelAttribute("vo") WebBoard vo) {
+    log.info("register get");
+  }
+
+  @PostMapping("/boards/register")
+  public String registerPOST(@ModelAttribute("vo") WebBoard vo, RedirectAttributes rttr) {
+    log.info("register post");
+    log.info("" + vo);
+    repo.save(vo);
+    rttr.addFlashAttribute("msg", "success");
+    return "redirect:/boards/list";
   }
 
   @GetMapping("/sample")
