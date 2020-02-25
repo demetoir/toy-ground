376f161f09eb1a8272602cdfe40c9072b726c7a4
demetoir-destop <wnsqlehlswk@naver.com>
2020-02-26 05:14:54 +0900

    add: spring example 더미 member insert 용 unit test 추가

---
+++ start-spring-boot/src/test/java/com/example/demetoir/domain/MemberTest.java
@@ -0,0 +1,74 @@
+package com.example.demetoir.domain;
+
+import com.example.demetoir.persistance.MemberRepository;
+import lombok.extern.java.Log;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.annotation.Commit;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Log
+@Commit
+public class MemberTest {
+
+  @Autowired private MemberRepository memberRepository;
+
+  @Autowired private PasswordEncoder passwordEncoder;
+
+  @Test
+  public void testInsert() {
+    for (int i = 0; i < 100; i++) {
+      Member member = new Member();
+
+      member.setUid("user" + i);
+      member.setUpw("pw" + i);
+      member.setUname("user" + i);
+
+      MemberRole role = new MemberRole();
+      if (i <= 80) {
+        role.setRoleName("BASIC");
+      } else if (i <= 90) {
+        role.setRoleName("MANAGER");
+      } else {
+        role.setRoleName("ADMIN");
+      }
+      member.setRoles(Collections.singletonList(role));
+
+      memberRepository.save(member);
+    }
+  }
+
+  @Test
+  public void testUpdateOldMember() {
+    List<String> ids = new ArrayList<>();
+
+    for (int i = 0; i < 100; i++) {
+      ids.add("user" + i);
+    }
+
+    memberRepository
+        .findAllById(ids)
+        .forEach(
+            member -> {
+              member.setUpw(passwordEncoder.encode(member.getUpw()));
+              memberRepository.save(member);
+            });
+  }
+
+  @Test
+  public void testRead() {
+    Optional<Member> result = memberRepository.findById("user43");
+
+    result.ifPresent(member -> log.info("member " + member));
+  }
+}
