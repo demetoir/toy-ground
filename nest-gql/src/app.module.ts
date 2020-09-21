257bf49ba0b704c3a1e1a8d136f6023b29907cb1
demetoir <wnsqlehlswk@naver.com>
2020-09-21 21:31:07 +0900

    init: nest-gql example

---
+++ nest-gql/src/app.module.ts
@@ -0,0 +1,17 @@
+import {Module} from '@nestjs/common';
+import {GraphQLModule} from "@nestjs/graphql";
+import {join} from 'path';
+import {AuthorModule} from "./author/author.module";
+
+@Module({
+    imports: [
+        AuthorModule,
+        GraphQLModule.forRoot({
+            autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+            sortSchema: true,
+        }),
+
+    ],
+})
+export class AppModule {
+}
