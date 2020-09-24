6ac12fc6030a2fe4668bd0227ba44fa9300e0bca
demetoir <wnsqlehlswk@naver.com>
2020-09-24 23:23:40 +0900

    doc: add nest-gql know how comment

--- nest-gql/src/author/author.resolver.ts
+++ nest-gql/src/author/author.resolver.ts
@@ -13,6 +13,12 @@ async function getShit(post) {
     return shit;
 }
 
+// Query나 mutation 데콘는 어디에 들어가도 딱히 상관없는듯하다
+// 다만 ResolveField 데코는 해당하는 @Parent 에 해당하는 @Resolver 내부에서 선언되어야
+// gql상에서 traversal이 가능하다
+// @Root 는 확인 해봐야한다
+// @Resolver 에서는 무조건 타입을 지정하고 해당 타입을 root로 삼는 모든 query, mutation 를 정의한다
+// 그리고 해당 타입에 해당하는 @Parent 인자를 가지는 @ResolveField 를 정의한다
 @Resolver(() => Author)
 export class AuthorsResolver {
     // private authorsService: AuthorsService,
@@ -41,6 +47,7 @@ export class AuthorsResolver {
         return author;
     }
 
+    // notice Resolver
     @ResolveField(() => [Post], {name: 'posts'})
     async posts(
         @Parent()
