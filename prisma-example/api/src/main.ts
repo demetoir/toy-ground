23704561a1e43ee8dda16c7ad7f876b654701152
demetoir <wnsqlehlswk@naver.com>
2020-09-28 01:25:30 +0900

    chore: add .env

--- prisma-example/api/src/main.ts
+++ prisma-example/api/src/main.ts
@@ -2,7 +2,9 @@
 // import {PrismaClient, User, UserCreateInput} from "../prisma/client"
 // todo generated 된 package.json의 name을 @wrap/prisma로 변경해야한다
 //  그래야 webstorm에서 임포트시 정적분석이 된다
-import {PrismaClient, User, UserCreateInput} from "@wrap/prisma"
+// 인줄 알았는데 해당 generated 된 폴더를 un exclude시키니 잘나온다
+// https://intellij-support.jetbrains.com/hc/en-us/community/posts/360009385399-Go-to-declaration-auto-import-doesn-t-work-in-certain-situations
+import {PrismaClient, User, UserCreateInput} from "@prisma/client"
 
 (async function () {
     const prisma: PrismaClient = new PrismaClient()
