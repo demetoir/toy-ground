0bdbd0da83b6b8912285d4526af0331af6c074f4
demetoir <wnsqlehlswk@naver.com>
2020-09-28 01:03:39 +0900

    feat: add hacked prisma generator ja

--- prisma-example/api/src/main.ts
+++ prisma-example/api/src/main.ts
@@ -1,5 +1,18 @@
-import { PrismaClient } from '@prisma/client'
-// const { PrismaClient } = require('@prisma/client')
+// import {PrismaClient, User, UserCreateInput} from '../.prisma/client'
+// import {PrismaClient, User, UserCreateInput} from "../prisma/client"
+// todo generated 된 package.json의 name을 @wrap/prisma로 변경해야한다
+//  그래야 webstorm에서 임포트시 정적분석이 된다
+import {PrismaClient, User, UserCreateInput} from "@wrap/prisma"
 
-const prisma = new PrismaClient()
+(async function () {
+    const prisma: PrismaClient = new PrismaClient()
 
+    const userInput: UserCreateInput = {
+        name: "12312",
+        email: "123123"
+    }
+
+    const user: User = await prisma.user.create({data: userInput})
+
+    console.log(user)
+})()
