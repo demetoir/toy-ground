f9fb5fb6cd61c0c3d005a27258ca4fa63ba41732
demetoir <wnsqlehlswk@naver.com>
2021-01-01 15:34:44 +0900

    feat: add basic node app

---
+++ aws-eb-docker-deploy/app.js
@@ -0,0 +1,14 @@
+const express = require('express')
+
+
+const app = express();
+
+
+app.use('*', (req, res)=>{
+    res.send('hello shit world')
+})
+
+
+app.listen(3000, () => {
+    console.log('server listen')
+})
